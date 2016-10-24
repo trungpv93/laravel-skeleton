9f648a8274317d001f3866e0d245eb97724f8a19
trungpv <trungpv@Trungs-MacBook-Pro.local>
2016-10-24 23:08:25 +0700

    update namspace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -70,10 +70,12 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::group([
             'middleware' => 'api',
-            'namespace' => $this->namespace,
+            'namespace' => $this->namespace."\\API",
             'prefix' => 'api',
+            'as' => 'api.',
         ], function ($router) {
             require base_path('routes/api.php');
-        });
+        });    
+
     }
 }
